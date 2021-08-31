4052ced546200c6d023fe73b605a96adaab879aa
Keri Price <keri.lynne.p@gmail.com>
2021-08-31 12:45:15 -0400

    updated script.js and created  results.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
-var
+var searchEl = document.querySelector('#search-form');
+
+function formSubmit(event) {
+    event.preventDefault();
+
+    var input = document.querySelector('#search-input');
+    var criteria = document.querySelector('#search-criteria');
+
+    var queryString = './search-results.html?q=' + input + '&criteria=' + criteria;
+
+    location.assign(queryString);
+}
+
+searchEl.addEventListener('submit', formSubmit);
