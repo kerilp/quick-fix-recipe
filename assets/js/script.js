9fc214f4724a0dcbb2bfbbdc0436d33b314d36e0
Keri Price <keri.lynne.p@gmail.com>
2021-08-31 13:14:25 -0400

    fixed errors in script.js and results.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,13 @@ var searchEl = document.querySelector('#search-form');
 function formSubmit(event) {
     event.preventDefault();
 
-    var input = document.querySelector('#search-input');
-    var criteria = document.querySelector('#search-criteria');
+    var input = document.querySelector('#search-input').value;
+    var criteria = document.querySelector('#search-criteria').value;
+
+    if(!input || !criteria) {
+        // does not search
+        // displays modal telling user they must enter a search term
+    }
 
     var queryString = './search-results.html?q=' + input + '&criteria=' + criteria;
 
